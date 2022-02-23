67f95511eb3a7cdf07f933ca5b0fc36d915ce5a1
Zanal <standakuna@seznam.cz>
2022-02-23 20:12:04 +0100

    Update Bakalarka.m

--- Bakalarka.m
+++ Bakalarka.m
@@ -1,6 +1,10 @@
 close all
 clear
 
+runThesis();
+
+% Function for 
+function runThesis()
 str = input("Do you want to change default setting? Y or enter: ",'s');
 if ~isempty(str)
     newN = input("Set number of points: "); % check if integer
@@ -17,22 +21,26 @@ end
 [pointsArr, heightList] = generateEnvironment(smoothVal, n, height);
 [r, c] = find(pointsArr > -1);
 bools = pointsArr > -1;
-coords = [r, c, pointsArr(bools)]; % almost x,y,z
+coords = [r, c, pointsArr(bools)]; % x,y,z
 
 triangArr = createTriangulations(r, c);
 [lineList, lLwCoords] = createListOfLines(coords, triangArr);
 sLWCoords = sortrows(lLwCoords, 'ascend');
 smootherpoints = smoothenPlane(coords, sLWCoords); % EACH POINTS NEEDS TRIANGLE TO FIND HEIGHT
-% CARTESIAN PRODUCT NOW
 cartesianPoints = makeCartesianProduct(smootherpoints);
-cartesianPoints = [cartesianPoints, ones(size(cartesianPoints,1), 1)];
+cartesianPoints = [cartesianPoints, -ones(size(cartesianPoints,1), 1)];
 [~, indx] = intersect(cartesianPoints(:,1:2), smootherpoints(:,1:2), 'rows');
 cartesianPoints(indx, 3) = smootherpoints(:, 3);
-disp(cartesianPoints);
-% FIND BELONG TRIANGLE - for all triangles search remaining points
-displayTriangulated(triangArr, heightList, r, c, cartesianPoints, pointsArr);
+tic
+[cTriangleVariables, triangleEquations] = calculateTriangEq(triangArr, coords);
+cartesianPoints = calculateCPHeights(triangArr, coords, cartesianPoints, triangleEquations, cTriangleVariables);
+toc;
+%disp(cartesianPoints);
+displayTriangulated(triangArr, heightList, r, c, cartesianPoints);
 disp("DONE");
+end
 
+% OK
 % Sets required variables and checks valid values
 function [n, smoothVal, numRuns, height]  = setVariables(newN, newSmooth, newRuns, newHeight)
     n = checkValidity(newN, 4);
@@ -41,15 +49,19 @@ function [n, smoothVal, numRuns, height]  = setVariables(newN, newSmooth, newRun
     height = checkValidity(newHeight, 0);
 end
 
+% OK
 % Check input validity - NEED TO ADD STRINGS/FLOATS ...
 function checkedValue = checkValidity(valueWanted, threshold)
     if valueWanted <= threshold
+       tooLow = ["Value too low setting to ", threshold];
+       disp(tooLow)
        checkedValue = threshold;
     else
        checkedValue = valueWanted;
     end
 end
 
+% OK - testing in place 76, 79 [comment for random]
 % Returns list of point heights with a 2D array with them marked
 function [pointsArr, heightList] = generateEnvironment(smoothVal, n, height)
     % create appropriate 2d array
@@ -61,21 +73,23 @@ function [pointsArr, heightList] = generateEnvironment(smoothVal, n, height)
         (nIdx - nRC + 2):(nIdx - 1)]; % last row
     cornerIdx = [1, nRC, nIdx - nRC + 1, nIdx];
     idx = sort([noCornerIdx(randperm(numel(noCornerIdx), n - 4)), cornerIdx]); % Matlab indexes from 1, maybe sort maybe no
-    idx = [3,7,8,12,13,19, cornerIdx]; % testing crosses
-    idx = [1, 3, nRC, 7, 8, 12, 13, 19, nIdx - nRC + 1, nIdx];
+    %idx = [1, 3, nRC, 7, 8, 12, 13, 19, nIdx - nRC + 1, nIdx]; % TESTING
     heightRange = height + 1;
     heightList = randperm(heightRange, n) - 1;
+    %heightList = [4,41,34,15,44,1,19,16,32,33]; % TESTING
     pointsArr(idx) = heightList; % can be 0
 end
 
+% OK
 % Returns delaunay triangulation using delaunay library
 function triangArr = createTriangulations(r, c)
     % create triangulation array from n and smoothVal
     triangArr = delaunay(r, c);
 end
 
+% OK
 % Displays either 2D, 3D, or both
-function displayTriangulated(triangArr, heightList, r, c, cartesianPoints, pointsArr)
+function displayTriangulated(triangArr, heightList, r, c, cartesianPoints)
     % display triangulated place
     str = input("Do you want 2D, 3D, or both visualizations? 2,3, or enter: ", 's');
     if (strcmp(str,"2"))
@@ -86,7 +100,7 @@ function displayTriangulated(triangArr, heightList, r, c, cartesianPoints, point
     elseif (strcmp(str,"3"))    
         trimesh(triangArr, r, c, heightList);
         hold on
-        plot(cartesianPoints(:, 1), cartesianPoints(:, 2), 'r.') % not enough
+        plot3(cartesianPoints(:, 1), cartesianPoints(:, 2), cartesianPoints(:, 3), 'r.') % not enough
         hold off
     else
         triplot(triangArr, r, c);
@@ -101,6 +115,7 @@ function displayTriangulated(triangArr, heightList, r, c, cartesianPoints, point
     end
 end
 
+% OK
 % Function returns lineList = list of lines with coords indices and
 % lLwCoords = array of line coordinations (x1, y1, x2, y2)
 function [lineList, lLwCoords] = createListOfLines(coords, triangArr)
@@ -121,61 +136,126 @@ function [lineList, lLwCoords] = createListOfLines(coords, triangArr)
     end
 end
 
-function probDist = evaluateZeroSumGame(pointsUtils)
-    % evaluate game
-end
-
+% OK
+% Find new intersections
 function smootherpoints = smoothenPlane(coords, sLWCoords)
     % create more points x, y % Gets called
-    smootherpoints = coords
+    smootherpoints = coords;
     for i = 1:size(coords, 1) % Runs correctly 10 times
-        y =coords(i,:);
-        x = ['Point ', num2str(y), ' has: '];
-        disp(x);
-        verticalInt = sLWCoords((sLWCoords(:,1)<coords(i,1) & sLWCoords(:,4)>coords(i,1)) | (sLWCoords(:,4)<coords(i,1) & sLWCoords(:,1)>coords(i,1)), :);
-        %disp(verticalInt);
+        verticalInt = sLWCoords((sLWCoords(:,1)<coords(i,1) & sLWCoords(:,4)>coords(i,1)) | ...
+                        (sLWCoords(:,4)<coords(i,1) & sLWCoords(:,1)>coords(i,1)), :);
         % make points from his X same, calculate Y
         for j = 1:size(verticalInt, 1)
             if verticalInt(j,2) == verticalInt(j,5)
-                newVerticalP = [coords(i,1), verticalInt(j,2), -1]
+                newVerticalP = [coords(i,1), verticalInt(j,2), -1];
             else
                 m = (verticalInt(j,2)-verticalInt(j,5)) / (verticalInt(j,1)-verticalInt(j,4));
                 c = verticalInt(j,2) - m * verticalInt(j,1);
-                newVerticalP = [coords(i,1), m * coords(i,1) + c, -1]
+                newVerticalP = [coords(i,1), m * coords(i,1) + c, -1];
             end
+            newVerticalP(3) = calculateHeights(verticalInt(j,:), newVerticalP);
             smootherpoints = cat(1, smootherpoints, newVerticalP);
         end
-        horizontalInt = sLWCoords((sLWCoords(:,2)<coords(i,2) & sLWCoords(:,5)>coords(i,2)) | (sLWCoords(:,5)<coords(i,2) & sLWCoords(:,2)>coords(i,2)), :);
-        %disp(horizontalInt);
+        horizontalInt = sLWCoords((sLWCoords(:,2)<coords(i,2) & sLWCoords(:,5)>coords(i,2)) | ...
+                            (sLWCoords(:,5)<coords(i,2) & sLWCoords(:,2)>coords(i,2)), :);
         for j = 1:size(horizontalInt, 1)
             if horizontalInt(j,1) == horizontalInt(j,4)
-                newHorizontalP = [horizontalInt(j,1), coords(i,2), -1]
+                newHorizontalP = [horizontalInt(j,1), coords(i,2), -1];
             else
                 m = (horizontalInt(j,2)-horizontalInt(j,5)) / (horizontalInt(j,1)-horizontalInt(j,4));
                 c = horizontalInt(j,2) - m * horizontalInt(j,1);
-                newHorizontalP = [(coords(i,2) - c) / m , coords(i,2), -1]
+                newHorizontalP = [(coords(i,2) - c) / m , coords(i,2), -1];
             end
+            newHorizontalP(3) = calculateHeights(horizontalInt(j,:), newHorizontalP);
             smootherpoints = cat(1, smootherpoints, newHorizontalP);
         end
-        % make points from his X calculate, same Y
-        %newHorizontalP =  [ , coords(i,2)];
-        %smootherpoints = [smootherpoints, ...]
     end
-    %disp(smootherpoints);
     smootherpoints = round(smootherpoints, 4);
-    disp(smootherpoints); % OK
     smootherpoints = unique(smootherpoints, 'rows');
-    disp(smootherpoints);
 end
 
+% OK
+% Function used to calculate height of points found by intersection
+function calcedHeight = calculateHeights(linePoints, foundPoint)
+    if linePoints(3) < linePoints(6)
+        smallerCoords = linePoints(1:2);
+        smallerH = linePoints(3);
+    else
+        smallerCoords = linePoints(4:5);
+        smallerH = linePoints(6);
+    end
+    calcedHeight = smallerH + norm(foundPoint(1:2)-smallerCoords) / ...
+        norm(linePoints(1:2)-linePoints(4:5)) * (abs(linePoints(3)-linePoints(6)));
+end
+
+% OK
+% Creates Cartesian product from unique x and y coordinates
 function cartesianPoints = makeCartesianProduct(smootherpoints)
     xn = unique(smootherpoints(:, 1), 'rows');
     yn = unique(smootherpoints(:, 2), 'rows');
     cartesianPoints = combvec(xn.', yn.').';
 end
 
+% Using pre-calculated equations find triangle belonging to each unassigned
+% point and assign them height
+function cartesianPoints = calculateCPHeights(triangArr, coords, cartesianPoints, triangleEquations, cTriangleVariables)
+    noHeightIdx = find(cartesianPoints(:,3) == -1);
+    for i = 1:length(noHeightIdx)
+       for j = 1:length(triangArr)
+            p1 = coords(triangArr(j,1), :); % x, y 1
+            p2 = coords(triangArr(j,2), :); % x,y 2
+            p3 = coords(triangArr(j,3), :); % x,y 3
+            chp = cartesianPoints(noHeightIdx(i), 1:2); % x,y checked
+            c1 = cTriangleVariables(j,1) * (chp(2) - p1(2)) - cTriangleVariables(j,2) * (chp(1) - p1(1));
+            c2 = cTriangleVariables(j,3) * (chp(2) - p2(2)) - cTriangleVariables(j,4) * (chp(1) - p2(1));
+            c3 = cTriangleVariables(j,5) * (chp(2) - p3(2)) - cTriangleVariables(j,6) * (chp(1) - p3(1)); % OK
+            if (c1 <= 0 && c2 <= 0 && c3 <= 0) || (c1 >= 0 && c2 >= 0 && c3 >= 0)
+                cartesianPoints(noHeightIdx(i), 3) = p1(3) + triangleEquations(j,1) / triangleEquations(j,2) ...
+                        * (chp(2) - p1(2)) - triangleEquations(j,3) / triangleEquations(j,4) * (chp(1) - p1(1));                
+                break % Above needs y = Ax + b 
+            end
+       end    
+    end
+    
+end
+
+% Possible usage in search empty now
 function newPoints = checkHorizontal()
 end
 
+% Possible usage in search empty now
 function newPoints = checkVertical()
+end
+
+% Calculate complex parts for triangle equations ahead to save number of
+% computations later
+function [cTriangleVariables, triangleEquations] = calculateTriangEq(triangArr, coords)
+    % maybe also c1,c2,c3 parts
+    triangleEquations = zeros(length(triangArr), 4);
+    cTriangleVariables = zeros(length(triangArr), 6);
+    for i = 1:length(triangArr)
+        p1 = coords(triangArr(i,1), :); % x,y 1
+        p2 = coords(triangArr(i,2), :); % x,y 2
+        p3 = coords(triangArr(i,3), :); % x,y 3        
+        cTriangleVariables(i,1) = (p2(1) - p1(1));
+        cTriangleVariables(i,2) = (p2(2) - p1(2));
+        cTriangleVariables(i,3) = (p3(1) - p2(1));
+        cTriangleVariables(i,4) = (p3(2) - p2(2));
+        cTriangleVariables(i,5) = (p1(1) - p3(1));
+        cTriangleVariables(i,6) = (p1(2) - p3(2));
+        triangleEquations(i,1) = ((p2(1)-p1(1))*(p3(3)-p1(3)) - (p3(1)-p1(1))*(p2(3)-p1(3))); % function for all triangles to avoid repetative calculation
+        triangleEquations(i,2) = ((p2(1)-p1(1))*(p3(2)-p1(2)) - (p3(1)-p1(1))*(p2(2)-p1(2)));
+        triangleEquations(i,3) = ((p2(2)-p1(2))*(p3(3)-p1(3)) - (p3(2)-p1(2))*(p2(3)-p1(3)));
+        triangleEquations(i,4) = ((p2(1)-p1(1))*(p3(2)-p1(2)) - (p3(1)-p1(1))*(p2(2)-p1(2)));
+    end
+end
+
+% Calculate game probabilities
+function probDist = evaluateZeroSumGame(pointsUtils)
+    % evaluate game
+    % TODO
+    % maybe utility matrix needed
+    % probably linprog()
+    % have to know where the point results are as before I knew the ID and
+    % not the coords
 end
